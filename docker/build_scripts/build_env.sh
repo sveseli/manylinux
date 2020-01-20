69e0c81d38354eb328859bf864be53d5c9152aa7
Sinisa Veseli <sveseli@apswl60-146.aps.anl.gov>
2020-01-20 14:12:07 -0600

    updated python versions

--- docker/build_scripts/build_env.sh
+++ docker/build_scripts/build_env.sh
@@ -1,7 +1,7 @@
 # source me
 
 PYTHON_DOWNLOAD_URL=https://www.python.org/ftp/python
-CPYTHON_VERSIONS="2.7.17 3.4.10 3.5.9 3.6.9 3.7.5 3.8.0"
+CPYTHON_VERSIONS="2.7.17 3.4.10 3.5.9 3.6.10 3.7.6 3.8.1"
 
 # openssl version to build, with expected sha256 hash of .tar.gz
 # archive.
